658b3ba75df5b162760038d6e188255d09276b92
Hannah Rice <riceh3@uni.coventry.ac.uk>
2016-11-20 16:21:22 +0000

    Add files via upload

--- trailing_zeroes.py
+++ trailing_zeroes.py
@@ -15,6 +15,8 @@ def factorial_trailing_zeroes(n):
            # (1*1 = 1), (1*2 = 2), (2*3 = 6), (6*4 = 24), etc...
 
            calc = (5**(i-1)) # Determines trailing zeroes
+           # eg: (5**0, 5**1, 5**2, 5**3, 5**4)
+           print(calc)
            calc = int(calc)
            ans = (factorial/calc)
 
