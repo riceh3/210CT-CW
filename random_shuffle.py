975c3506fe533f82a4048c0213fff3aa8bac2f8a
Hannah Rice <riceh3@uni.coventry.ac.uk>
2016-11-27 12:32:53 +0000

    Add files via upload

--- random_shuffle.py
+++ random_shuffle.py
@@ -5,25 +5,28 @@ def random_shuffle(A,shuffled,length):
 
     """ Function to randomly shuffle an array of integers """
 
-    a = random.choice(A)
+    a = random.choice(A)            # Random value from list A
     
     while length > 0:               # Base Case for recursion
 
-        if a in shuffled:
+        if a in shuffled:           # Cannot have duplicate values
+            
             A.remove(a)           
             random_shuffle(A,shuffled,length)
             return shuffled
         
         else:
+            
             shuffled.append(a)
-            length = length-1
+            length = length-1       # Moves towards base case
+            
             random_shuffle(A,shuffled,length)
             return shuffled
 
 try:                                # Check for invalid input to avoid errors.
     
-    A = ([1,3,4,6,7,2])             # Input
-    shuffled = []
+    A = ([1,3,4,6,7,2])             
+    shuffled = []                   # New list to store randomly shuffled input
     length = len(A)
 
     random_shuffle(A,shuffled,length)
@@ -34,6 +37,7 @@ except NameError:
     sys.exit()
     
 except IndexError:
+    
     print("Invalid input: Must only be one of each number")
     sys.exit()
 
