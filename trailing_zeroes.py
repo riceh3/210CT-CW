8af0c242314e76ba82acfae35e9435d6b0e0bc41
Hannah Rice <riceh3@uni.coventry.ac.uk>
2016-11-27 12:38:37 +0000

    Add files via upload

--- trailing_zeroes.py
+++ trailing_zeroes.py
@@ -2,14 +2,14 @@ import sys
 
 def factorial_trailing_zeroes(n):
 
-    """ Function first detemines the factorial of input """
+    """ Function first determines the factorial of input """
     """ Then calculates the number of trailing zeroes """
 
     if n > 0:                          # A factorial number must be greater than 0
 
         factorial = 1
 
-        for i in range(1, n+1):        # Loop range detemined by input
+        for i in range(1, n+1):        # Loop range determined by input
 
            factorial = (factorial*i)   # Determines the factorial of input
 
