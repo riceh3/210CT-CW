091e6518d7f0fbc05814a1b1677c4b97a594911e
Hannah Rice <riceh3@uni.coventry.ac.uk>
2016-11-28 10:19:14 +0000

    Add files via upload

--- graph_weighted_Dijkstra_algorithm.py
+++ graph_weighted_Dijkstra_algorithm.py
@@ -1,105 +1,104 @@
 
-#my_graph = {'1' : ['2'(5)],   # own list of directly connected vertexes
+#my_graph = {'1' : ['2'(5)],   
    #         '2' : ['1'(5),'4'(7)],
     #        '3' : ['4'(4),'5'(8)],
      #       '4' : ['2'(7),'3'(4),'5'(3)],
       #      '5' : ['3'(8),'4'(3)] }
+      
 import sys
+
 class Graph:
+
+    """ Insert given vertex and edges into graph """
 
     def __init__(self):
 
-        vertices = {}
+        vertices = {}               # Used a dictionary to store vertex as key
         self.vertices = vertices
-        visited = []
+        visited = []     
         self.visited = visited
 
-    def add_vertex(self,new_vertex):  # Add vertex = add key
+    def add_vertex(self,new_vertex):
+
+        """ Add vertex as key """
 
         self.vertices[new_vertex] = []
 
     def add_edge(self, new_vertex, new_edge, weight):
+
+        """ Add edge and weight """
 
         self.vertices.setdefault(new_vertex, []).append(new_edge)
         self.vertices[new_vertex].append(weight)
         
     def show_graph(self):
 
+        """ Display the graph """
+
         print(self.vertices)
 
-    def find_weight(self):
-
-        current = self.vertices['2']
-        print(current)
-
-        weight = []
-
-        for t in current:
-
-            if type(t) == int:
-                weight.append(t)
-
-            else:
-                print("")
-
-        print("Weight")
-        print(weight)
-
     def dijkstra(self, start, destination, visited, weight):
-        v = []
 
-        if start not in visited:
+        """ Find the shortest path using the weights of each edge """
+        
+        edge = []
 
-            if start != destination:
+        if start not in visited:                  # If not already visited vertex 
+
+            if start != destination:              
             
-                visited.append(start)
+                visited.append(start)             # Add current vertex to visited list
                 print("")
                 print("Visited")
                 print(visited)
 
-                search = self.vertices[start]
+                search = self.vertices[start]     # Access edges to current vertex 
 
-                for w in search:
+                for w in search:                  # Find the weights 
 
-                    if type(w) == int:
-                        weight.append(w)
+                    if type(w) == int:            # Weights are integers 
+                        weight.append(w)          # Put weights into list 
 
                     else:
-                        v.append(w)
+                        edge.append(w)            # Put edges into list 
                         print("")
 
-                min_weight = min(weight)
+                min_weight = min(weight)          # Find lowest weight from list 
 
-                weight.clear()
+                weight.clear()                    
 
-                weight.append(min_weight)
+                weight.append(min_weight)         # Only store the min weight
+
+                # The edge before min weight will be our next start vertex
 
                 if weight[0] == search[1]:
-                    a = str(v[0])
+                    
+                    a = str(edge[0])
                     start = a
                     g.dijkstra(start, destination, visited, weight)
                     return start
                 
 
                 elif weight[0] == search[3]:
-                    start = v[1]
+                    start = edge[1]
                     g.dijkstra(start, destination, visited, weight)
                     return start
 
                 elif weight[0] == search[5]:
-                    start = v[2]
+                    start = edge[2]
                     g.dijkstra(start, destination, visited, weight)
                     return start
 
             else:
-                print(self.visited)
-                sys.exit()
+                
+                return start 
         else:
-            print("The shorted path")
+            print("The shortest path : ")
             print(visited)
             sys.exit()
   
-g = Graph()
+g = Graph()                                  # Graph for visual representation:
+
 g.add_vertex('1')                            #       1 --- 2 --- 4 --- 5
 g.add_vertex('2')                            #                   |    /
 g.add_vertex('3')                            #                   3 - /  
@@ -109,7 +108,7 @@ g.add_vertex('5')
 g.show_graph()
 print("")
 
-weight = (5)
+weight = (5)                                 # Add edges and weights 
 g.add_edge('1',('2'),weight)
 weight = (5)
 g.add_edge('2',('1'), weight)
@@ -133,17 +132,12 @@ g.add_edge('5',('4'), weight)
 
 g.show_graph()
 
-start = '1'
-destination = '3'
+start = '1'                               # Start path 
+destination = '3'                         # End path 
 weight = []
 visited = []
 g.dijkstra(start, destination, visited, weight)
 
-#g.find_weight()
-#print("")
-#g.show_graph()
-#print("")
-#start = '1'
 
 
 
